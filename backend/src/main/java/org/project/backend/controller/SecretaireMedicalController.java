d5e6349d8dd06313a94b60e53f4a649a59870c59
yassir <fahimiyassir4@gmail.com>
2025-04-22 16:34:14 +0100

    add secretaire delete and update

--- backend/src/main/java/org/project/backend/controller/SecretaireMedicalController.java
+++ backend/src/main/java/org/project/backend/controller/SecretaireMedicalController.java
@@ -1,11 +1,21 @@
 package org.project.backend.controller;
 
+import org.project.backend.entities.SecretaireMedical;
 import org.project.backend.service.SecretaireMedicalService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(("/api/secretaires"))
 public class SecretaireMedicalController {
     @Autowired
     SecretaireMedicalService secretaireMedicalService;
+    @DeleteMapping("/{id}")
+    public void deleteSecretaireMedical(@PathVariable Long id) {
+        secretaireMedicalService.deleteSecretaireMedical(id);
+    }
+    @PutMapping("/{id}")
+    public SecretaireMedical getUpdateSecretaireMedical(@PathVariable Long id, @RequestBody SecretaireMedical updateSecretaireMedical) {
+        return secretaireMedicalService.updateSecretaireMedical(id, updateSecretaireMedical);
+    }
 }
--- backend/src/main/java/org/project/backend/service/SecretaireMedicalService.java
+++ backend/src/main/java/org/project/backend/service/SecretaireMedicalService.java
@@ -1,11 +1,29 @@
 package org.project.backend.service;
 
+import org.project.backend.entities.SecretaireMedical;
 import org.project.backend.repository.SecretaireMedicalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class SecretaireMedicalService {
     @Autowired
     SecretaireMedicalRepository secretaireMedicalRepository;
-}
+    public void deleteSecretaireMedical(Long id) {
+        secretaireMedicalRepository.deleteById(id);
+    }
+    public SecretaireMedical updateSecretaireMedical(Long id, SecretaireMedical updatedSecretaireMedical) {
+        SecretaireMedical existingSecretaireMedical = (SecretaireMedical) secretaireMedicalRepository.getSecretaireMedicalById(id);
+        if (existingSecretaireMedical == null) {
+            return null;
+        } else {
+            existingSecretaireMedical.setName(updatedSecretaireMedical.getName());
+            existingSecretaireMedical.setPrenom(updatedSecretaireMedical.getPrenom());
+            existingSecretaireMedical.setDateNaissance(updatedSecretaireMedical.getDateNaissance());
+            return secretaireMedicalRepository.save(existingSecretaireMedical);
+        }
+    }
+        
+        
+    }
