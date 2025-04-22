0c14d4be4858f9949ec9d49ba0c3aa1044494326
yassir <fahimiyassir4@gmail.com>
2025-04-22 15:14:24 +0100

    fix some errors

--- backend/src/main/java/org/project/backend/controller/PatientController.java
+++ backend/src/main/java/org/project/backend/controller/PatientController.java
@@ -1,16 +1,18 @@
 package org.project.backend.controller;
 
 import org.project.backend.entities.Patient;
-import org.project.backend.repository.PatientRepository;
 import org.project.backend.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
-@RestController("/api/patients")
+@RestController
+@RequestMapping("/api/patients")
 class PatientController {
     @Autowired
     PatientService patientService;
@@ -20,9 +22,8 @@ class PatientController {
         return patientService.getAllPatients();
     }
 
-    @GetMapping({"/{id}"})
-    public Patient getPatient(Long id) {
+    @GetMapping("/{id}")
+    public Optional<Patient> getPatient(@PathVariable Long id) {
         return patientService.getPatientById(id);
     }
-
 }
--- backend/src/main/java/org/project/backend/service/PatientService.java
+++ backend/src/main/java/org/project/backend/service/PatientService.java
@@ -6,25 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PatientService {
+
     @Autowired
     private PatientRepository patientRepository;
 
     public List<Patient> getAllPatients() {
         return patientRepository.findAll();
     }
+
     public Patient addPatient(Patient patient) {
         return patientRepository.save(patient);
     }
-    public Patient getPatientById(Long id) {
-        return patientRepository.getPatientById(id);
+
+    public Optional<Patient> getPatientById(Long id) {
+        return patientRepository.findById(id);
     }
+
     public Patient updatePatient(Patient patient) {
         return patientRepository.save(patient);
     }
-    public void deletePatient(Long id) {
-        patientRepository.deleteById(id);
+
+    public boolean deletePatient(Long id) {
+        if (patientRepository.existsById(id)) {
+            patientRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
