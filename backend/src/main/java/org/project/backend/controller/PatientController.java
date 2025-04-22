227bc481e24d60a04a0fb4c7afe253bf2ca49bdc
Mohamed Tahiri <medtohairi@gmail.com>
2025-04-22 15:27:45 +0100

    add update and delete medcine methoud

--- backend/src/main/java/org/project/backend/controller/PatientController.java
+++ backend/src/main/java/org/project/backend/controller/PatientController.java
@@ -3,10 +3,7 @@ package org.project.backend.controller;
 import org.project.backend.entities.Patient;
 import org.project.backend.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
