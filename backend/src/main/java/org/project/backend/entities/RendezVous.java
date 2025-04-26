5a2239574585809b72d31204362e3c64624c8339
Mohamed Tahiri <medtohairi@gmail.com>
2025-04-26 22:34:31 +0100

    add get all rendez vous by id and medecin and patient methodes

--- backend/src/main/java/org/project/backend/entities/RendezVous.java
+++ backend/src/main/java/org/project/backend/entities/RendezVous.java
@@ -1,14 +1,20 @@
 package org.project.backend.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Date;
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
 public class RendezVous {
     @Id
     private int id;
     private Date date;
-    private boolean status;
+    private String status;
     @ManyToOne
     @JoinColumn(name = "medicin_id")
     private Medecin medecin;
--- backend/src/main/java/org/project/backend/service/RendezVousService.java
+++ backend/src/main/java/org/project/backend/service/RendezVousService.java
@@ -1,11 +1,26 @@
 package org.project.backend.service;
 
+import org.project.backend.entities.RendezVous;
 import org.project.backend.repository.RendezVousRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RendezVousService {
     @Autowired
     RendezVousRepository rendezVousRepository;
+
+    public List<RendezVous> getAllRendezVousByMedecin(Long medecinID) {
+        return rendezVousRepository.findAllByMedecinId(medecinID);
+    }
+
+    public List<RendezVous> getAllRendezVousByPatient(Long patientID) {
+        return rendezVousRepository.findAllByPatientId(patientID);
+    }
+
+    public RendezVous getRendezVousById(Long id) {
+        return rendezVousRepository.findById(id).orElse(null);
+    }
 }
